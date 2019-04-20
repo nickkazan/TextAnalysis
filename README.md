375ceee47d626fd833f29c7d2fe24d52f25d0159
Nick <nicholas.kazan1@gmail.com>
2019-04-20 09:39:00 -0400

    Read Me + clean up

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# TextAnalysis
+
--- main.py
+++ main.py
@@ -9,30 +9,6 @@ assert subscriptionKey
 
 textAnalyticsBaseUrl = "https://canadacentral.api.cognitive.microsoft.com/text/analytics/v2.0/"
 
-#The commented out portion is used for determining the language used in the text.
-#This is not useful to me as of right now.
-#
-# language_api_url = text_analytics_base_url + "languages"
-# print(language_api_url)
-#
-# documents = { 'documents': [
-#     { 'id': '1', 'text': 'This is the big man.' },
-#     { 'id': '2', 'text': 'Este es un document escrito en Español.' },
-#     { 'id': '3', 'text': '这是一个用中文写的文件' }
-# ]}
-#
-# headers   = {"Ocp-Apim-Subscription-Key": subscription_key}
-# response  = requests.post(language_api_url, headers=headers, json=documents)
-# languages = response.json()
-# pprint(languages)
-#
-# table = []
-# for document in languages["documents"]:
-#     text  = next(filter(lambda d: d["id"] == document["id"], documents["documents"]))["text"]
-#     langs = ", ".join(["{0}({1})".format(lang["name"], lang["score"]) for lang in document["detectedLanguages"]])
-#     table.append("<tr><td>{0}</td><td>{1}</td>".format(text, langs))
-# HTML("<table><tr><th>Text</th><th>Detected languages(scores)</th></tr>{0}</table>".format("\n".join(table)))
-
 sentimentApiUrl = textAnalyticsBaseUrl + "sentiment"
 documentsTest = {'documents' : [
   {'id': '1', 'text': 'I had a wonderful experience! The rooms were wonderful and the staff was helpful.'},
